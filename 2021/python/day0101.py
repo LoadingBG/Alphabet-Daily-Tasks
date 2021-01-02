1857b08e2baf0b09762fdd63092a78e8bd8fc87a
Gauravsingh <gauravsinghsisodia5@gmail.com>
2021-01-03 01:35:01 +0530

    Added day0101.py

---
+++ 2021/python/day0101.py
@@ -0,0 +1,23 @@
+import re
+
+nthEven = lambda n:2*n-2
+
+print(nthEven(1))
+print(nthEven(2))
+print(nthEven(100))
+print("-----")
+
+validateEmail = lambda s:bool(re.match(r".+@.+\..+", s))
+
+print(validateEmail("@gmail.com"))
+print(validateEmail("hello.gmail@com"))
+print(validateEmail("gmail"))
+print(validateEmail("hello@gmail"))
+print(validateEmail("hello@edabit.com"))
+print("-----")
+
+atbash=lambda  s:''.join([chr(90-(ord(x)-65)) if x.isalpha() & x.isupper() else (chr(122-(ord(x)-97) if x.isalpha() else ord(x))) for x in s])
+
+print(atbash("apple"))
+print(atbash("Hello world!"))
+print(atbash("Christmas is the 25th of December"))
